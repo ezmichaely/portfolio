0f59b7510d9002b25d80e362219fe614eeceedeb
EZ MICHAEL YUCOR <76212358+ezmichaely@users.noreply.github.com>
2023-11-01 18:44:33 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
     return [
       {
         source: "/lodong",
-        destination: "https://lodongsite.vercel.app",
+        destination: "lodongsite.vercel.app",
         permanent: true,
       },
     ];
