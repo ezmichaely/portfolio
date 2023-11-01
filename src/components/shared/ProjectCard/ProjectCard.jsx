23491a61e78667d3b7acb9d6c52005368146116e
EZ MICHAEL YUCOR <76212358+ezmichaely@users.noreply.github.com>
2023-11-01 18:19:14 +0800

    Update ProjectCard.jsx

--- src/components/shared/ProjectCard/ProjectCard.jsx
+++ src/components/shared/ProjectCard/ProjectCard.jsx
@@ -1,19 +1,18 @@
-// import Image from 'next/image'
+import Link from 'next/link'
 
 export default function ProjectCard({details}) {
   // console.log(details);
 
   return (
-    <div className='w-full bg-white p-4 rounded'>
+    <Link href="www.lodongsite.vercel.app" target='_blank'
+      className='w-full bg-white p-4 rounded'>
       {/* <Image src={props.logo} alt={props.key} height={300} /> */}
       <div>
         <h4 className='font-beaufort text-2xl font-bold text-mirage-dark'>
           {details.name}
         </h4>
-        
-
       </div>
 
-    </div>
+    </Link>
   )
 }
