2734467da095e9e4ce1ac8fb763846e1ca787901
EZ MICHAEL YUCOR <76212358+ezmichaely@users.noreply.github.com>
2023-11-11 01:11:04 +0800

    Update Main.jsx

--- src/components/sections/Main/Main.jsx
+++ src/components/sections/Main/Main.jsx
@@ -38,7 +38,7 @@ export default function Home() {
             </div>
 
             <div className={styles.resume}>
-              <a href="/EZ_MICHAEL_YUCOR-CV.pdf" > Resume / CV </a>
+              <a href="/EZ_MICHAEL_YUCOR-CV.pdf" target="_blank"> Resume / CV </a>
             </div>
           </div>
 
