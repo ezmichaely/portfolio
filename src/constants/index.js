3fc7b1bbcc457394832d85bb3d620a35978ea956
EZ MICHAEL YUCOR <76212358+ezmichaely@users.noreply.github.com>
2023-11-11 00:30:25 +0800

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -357,7 +357,7 @@ export const projectsData = [
     img: supermanDairy,
     desc: 'This system encompassed communication management, constructor and technician team oversight, order and contract management, technician scheduling, and a content management system. It even featured a raffle management system to award prizes to various tenants.',
     url: 'https://godofhouse.co.kr/',
-    stack: ['REACT JS', 'EXPRESS JS', 'NODE JS', 'FIREBASE', 'SOCKET.IO'],
+    stack: ['REACT.JS', 'EXPRESS.S', 'NODE.JS', 'FIREBASE', 'SOCKET.IO'],
     github: ''
   },
   // {
