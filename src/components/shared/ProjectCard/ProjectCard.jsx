0179272030261f8f2bf5bbf2d0b514d1d0168017
EZ MICHAEL YUCOR <76212358+ezmichaely@users.noreply.github.com>
2024-01-08 22:28:52 +0800

    patch

--- src/components/shared/ProjectCard/ProjectCard.jsx
+++ src/components/shared/ProjectCard/ProjectCard.jsx
@@ -7,33 +7,34 @@ export default function ProjectCard({ details }) {
   return (
     <div className={styles.card}>
       <div className={styles.cardContent}>
-        <div className={styles.imgHolder}>
-          <Image src={details.img} alt={details.name} />
-          <div className={styles.imgDesc}>
-            <h4> {details.name} </h4>
+        <div className="">
+          <div className={styles.imgHolder}>
+            <Image src={details.img} alt={details.name} />
+            <div className={styles.imgDesc}>
+              <h4> {details.name} </h4>
+            </div>
           </div>
         </div>
 
         <div className={styles.descriptions}>
           <div className={styles.descTop}>
-            {/* <h4> {details.name} </h4> */}
             <p>{details.desc}</p>
           </div>
-
           <div className={styles.descBot}>
             <div className={styles.links}>
-              {details.url &&
-                <Link href={details.url} target='_blank'>
-                LIVE SITE
+              {details.url && (
+                <Link href={details.url} target="_blank">
+                  LIVE SITE
                 </Link>
-              }
+              )}
 
-              {details.github &&
-                <Link href={details.github} target='_blank'>
+              {details.github && (
+                <Link href={details.github} target="_blank">
                   GITHUB
                 </Link>
-              }
+              )}
             </div>
+
             <div className={styles.stacks}>
               <h5>TECH USED:</h5>
 
@@ -46,9 +47,50 @@ export default function ProjectCard({ details }) {
               </div>
             </div>
           </div>
-
         </div>
+
+        {/* <div className="grow">
+          <div className={styles.imgHolder}>
+            <Image src={details.img} alt={details.name} />
+            <div className={styles.imgDesc}>
+              <h4> {details.name} </h4>
+            </div>
+          </div>
+          <div className='text-black mt-4'>
+            <p>{details.desc}</p>
+          </div>
+        </div>
+
+        <div className=''>
+          <div className={styles.descBot}>
+            <div className={styles.links}>
+              {details.url && (
+                <Link href={details.url} target="_blank">
+                  LIVE SITE
+                </Link>
+              )}
+
+              {details.github && (
+                <Link href={details.github} target="_blank">
+                  GITHUB
+                </Link>
+              )}
+            </div>
+
+            <div className={styles.stacks}>
+              <h5>TECH USED:</h5>
+
+              <div className={styles.tech}>
+                {details.stack.map((data, i) => (
+                  <div key={i} className={styles.techContent}>
+                    {data}
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div> */}
       </div>
     </div>
-  )
+  );
 }
