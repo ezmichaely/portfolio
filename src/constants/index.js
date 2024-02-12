d585ab98482752c30476879815ad48ff7b86e8e3
EZ MICHAEL YUCOR <76212358+ezmichaely@users.noreply.github.com>
2024-02-12 23:09:46 +0800

    restruct

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,154 +1,198 @@
 import {
-  facebook, gmail, github, linkedin, html5, css3, javascript, bootstrap, sass, tailwindcss, react, svelte, nextjs, node, jquery, typescript, cplusplus, java, php, laravel, python, xampp, mariadb, mysql, awsDynamodb, vscode, git, postman, figma, photoshop, githubLogo, mongodb, illustrator,
-
-  lodong, jungnanum, supermanDairy,
+  facebook,
+  gmail,
+  github,
+  linkedin,
+  html5,
+  css3,
+  javascript,
+  bootstrap,
+  sass,
+  tailwindcss,
+  react,
+  svelte,
+  nextjs,
+  node,
+  jquery,
+  typescript,
+  cplusplus,
+  java,
+  php,
+  laravel,
+  python,
+  xampp,
+  mariadb,
+  mysql,
+  awsDynamodb,
+  vscode,
+  git,
+  postman,
+  figma,
+  photoshop,
+  githubLogo,
+  mongodb,
+  illustrator,
+  lodong,
+  jungnanum,
+  supermanDairy,
   //
-
   bvp,
   desFiesta,
-  desXmas1, desXmas2,
-  dwightTarp, dwightTshirt,
-  infoSteamos, infoWin10,
-  logo_ducking, logo_farmigs,
-  logo_francyn, logo_rems,
+  desXmas1,
+  desXmas2,
+  dwightTarp,
+  dwightTshirt,
+  infoSteamos,
+  infoWin10,
+  logo_ducking,
+  logo_farmigs,
+  logo_francyn,
+  logo_rems,
   logo_tayoktok,
-  tarpBday, tarpFiesta,
-  tayok_drinks, tayok_menu,
-  tayok_opening, tayok_sessionistas,
-  tayok_tshirt, tayok_vibes,
-
-} from '@/assets/images';
+  tarpBday,
+  tarpFiesta,
+  tayok_drinks,
+  tayok_menu,
+  tayok_opening,
+  tayok_sessionistas,
+  tayok_tshirt,
+  tayok_vibes,
+} from "@/assets/images";
+import { norsuelcms } from "../assets/images";
 
 export const routers = [
   {
-    title: 'HOME',
-    url: '/',
-    icon: 'TbSmartHome'
+    title: "HOME",
+    url: "/",
+    icon: "TbSmartHome",
   },
   {
-    title: 'ABOUT',
-    url: '/about',
-    icon: 'TbUserHeart'
+    title: "ABOUT",
+    url: "/about",
+    icon: "TbUserHeart",
   },
   {
-    title: 'PROJECTS',
-    url: '/projects',
-    icon: 'TbCode'
+    title: "PROJECTS",
+    url: "/projects",
+    icon: "TbCode",
   },
   {
-    title: 'CONTACT',
-    url: '/contact',
-    icon: 'TbPhone'
-  },
-]
-
-export const footerDetails = [
-  {
-    title: 'Name',
-    content: 'EZ MICHAEL YUCOR'
-  },
-  {
-    title: 'Contact',
-    content: '+6392 6831 1909'
-  },
-  {
-    title: 'Email',
-    content: 'ezmichaely@gmail.com'
+    title: "CONTACT",
+    url: "/contact",
+    icon: "TbPhone",
   },
 ];
 
-export const copyRights = 'Copyright ⓒ 2023 | EZY. All Rights Reserved';
+export const footerDetails = [
+  {
+    title: "Name",
+    content: "EZ MICHAEL YUCOR",
+  },
+  {
+    title: "Contact",
+    content: "+6392 6831 1909",
+  },
+  {
+    title: "Email",
+    content: "ezmichaely@gmail.com",
+  },
+];
+
+export const copyRights = "Copyright ⓒ 2023 | EZY. All Rights Reserved";
 
 export const socials = [
   {
-    title: 'linkedin',
-    url: 'https://www.linkedin.com/in/ez-michael-yucor-283068204/',
-    icon: linkedin
+    title: "linkedin",
+    url: "https://www.linkedin.com/in/ez-michael-yucor-283068204/",
+    icon: linkedin,
   },
   {
-    title: 'github',
-    url: 'https://github.com/ezmichaely/',
-    icon: github
+    title: "github",
+    url: "https://github.com/ezmichaely/",
+    icon: github,
   },
   {
-    title: 'facebook',
-    url: 'https://www.facebook.com/ezmichaely/',
-    icon: facebook
+    title: "facebook",
+    url: "https://www.facebook.com/ezmichaely/",
+    icon: facebook,
   },
   {
-    title: 'gmail',
-    url: 'mailto:ezmichaely@gmail.com',
-    icon: gmail
+    title: "gmail",
+    url: "mailto:ezmichaely@gmail.com",
+    icon: gmail,
   },
 ];
 
 export const about = [
   {
-    title: 'intro',
-    content: "👋 Hello! I'm EZ Michael Yucor, a web developer passionate about creating captivating and functional websites. Proficient in HTML, CSS, JavaScript, React, Node.js, and MongoDB, I offer end-to-end web solutions.",
-    content2: "👋 Greetings! I'm EZ Michael Yucor, an enthusiastic web developer dedicated to crafting remarkable digital experiences. Proficient in HTML, CSS, and JavaScript, I specialize in designing visually captivating and highly functional websites. Whether it's harnessing the power of front-end frameworks like React or leveraging back-end tools such as Node.js and MongoDB, I possess the skills to provide end-to-end solutions."
+    title: "intro",
+    content:
+      "👋 Hello! I'm EZ Michael Yucor, a web developer passionate about creating captivating and functional websites. Proficient in HTML, CSS, JavaScript, React, Node.js, and MongoDB, I offer end-to-end web solutions.",
+    content2:
+      "👋 Greetings! I'm EZ Michael Yucor, an enthusiastic web developer dedicated to crafting remarkable digital experiences. Proficient in HTML, CSS, and JavaScript, I specialize in designing visually captivating and highly functional websites. Whether it's harnessing the power of front-end frameworks like React or leveraging back-end tools such as Node.js and MongoDB, I possess the skills to provide end-to-end solutions.",
   },
   {
-    title: 'design',
-    content: "🎨 With a focus on design, I create projects that function seamlessly and visually captivate users. My diverse portfolio highlights my commitment to collaboration, communication, and delivering exceptional results. Explore my work and let's discuss bringing your web development ideas to life!",
-    content2: "🎨 Placing a strong emphasis on design, I ensure that each project not only operates seamlessly but also captivates users aesthetically. My portfolio showcases a diverse array of projects, underscoring my commitment to collaborative work, effective communication, and consistently delivering exceptional outcomes. Feel free to explore my work, and don't hesitate to reach out so we can discuss how we might bring your web development visions to reality!"
-  }
+    title: "design",
+    content:
+      "🎨 With a focus on design, I create projects that function seamlessly and visually captivate users. My diverse portfolio highlights my commitment to collaboration, communication, and delivering exceptional results. Explore my work and let's discuss bringing your web development ideas to life!",
+    content2:
+      "🎨 Placing a strong emphasis on design, I ensure that each project not only operates seamlessly but also captivates users aesthetically. My portfolio showcases a diverse array of projects, underscoring my commitment to collaborative work, effective communication, and consistently delivering exceptional outcomes. Feel free to explore my work, and don't hesitate to reach out so we can discuss how we might bring your web development visions to reality!",
+  },
 ];
-
 
 export const techStacks = [
   {
-    key: 'html5',
-    name: 'HTML5',
-    url: 'https://www.w3.org/html/',
+    key: "html5",
+    name: "HTML5",
+    url: "https://www.w3.org/html/",
     icon: html5,
     height: 60,
     width: 60,
   },
   {
-    key: 'css3',
-    name: 'CSS3',
-    url: 'https://www.w3schools.com/css/',
+    key: "css3",
+    name: "CSS3",
+    url: "https://www.w3schools.com/css/",
     icon: css3,
     height: 60,
     width: 60,
   },
   {
-    key: 'js',
-    name: 'JAVASCRIPT',
-    url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/',
+    key: "js",
+    name: "JAVASCRIPT",
+    url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/",
     icon: javascript,
     height: 60,
     width: 60,
   },
   {
-    key: 'bootstrap',
-    name: 'BOOTSTRAP',
-    url: 'https://getbootstrap.com/',
+    key: "bootstrap",
+    name: "BOOTSTRAP",
+    url: "https://getbootstrap.com/",
     icon: bootstrap,
     height: 34,
     width: 40,
   },
   {
-    key: 'sass',
-    name: 'SASS',
-    url: 'https://sass-lang.com/',
+    key: "sass",
+    name: "SASS",
+    url: "https://sass-lang.com/",
     icon: sass,
     height: 30,
     width: 35,
   },
   {
-    key: 'tailwind',
-    name: 'TAILWIND',
-    url: 'https://tailwindcss.com/',
+    key: "tailwind",
+    name: "TAILWIND",
+    url: "https://tailwindcss.com/",
     icon: tailwindcss,
     height: 35,
     width: 38,
   },
   {
-    key: 'reactjs',
-    name: 'REACTJS',
-    url: 'https://reactjs.org/',
+    key: "reactjs",
+    name: "REACTJS",
+    url: "https://reactjs.org/",
     icon: react,
     height: 30,
     width: 30,
@@ -162,9 +206,9 @@ export const techStacks = [
   //   width: 30,
   // },
   {
-    key: 'nextjs',
-    name: 'NEXTJS',
-    url: 'https://nextjs.org/',
+    key: "nextjs",
+    name: "NEXTJS",
+    url: "https://nextjs.org/",
     icon: nextjs,
     height: 30,
     width: 30,
@@ -178,25 +222,25 @@ export const techStacks = [
   //   width: 30,
   // },
   {
-    key: 'jquery',
-    name: 'JQUERY',
-    url: 'https://jquery.com/',
+    key: "jquery",
+    name: "JQUERY",
+    url: "https://jquery.com/",
     icon: jquery,
     height: 30,
     width: 110,
   },
   {
-    key: 'typescript',
-    name: 'TYPESCRIPT',
-    url: 'https://www.typescriptlang.org/',
+    key: "typescript",
+    name: "TYPESCRIPT",
+    url: "https://www.typescriptlang.org/",
     icon: typescript,
     height: 30,
     width: 30,
   },
   {
-    key: 'cpp',
-    name: 'C++',
-    url: 'https://www.w3schools.com/cpp/',
+    key: "cpp",
+    name: "C++",
+    url: "https://www.w3schools.com/cpp/",
     icon: cplusplus,
     height: 30,
     width: 30,
@@ -210,17 +254,17 @@ export const techStacks = [
   //   width: 24,
   // },
   {
-    key: 'php',
-    name: 'PHP',
-    url: 'https://www.php.net/',
+    key: "php",
+    name: "PHP",
+    url: "https://www.php.net/",
     icon: php,
     height: 30,
     width: 50,
   },
   {
-    key: 'laravel',
-    name: 'Laravel',
-    url: 'https://laravel.com/',
+    key: "laravel",
+    name: "Laravel",
+    url: "https://laravel.com/",
     icon: laravel,
     height: 30,
     width: 30,
@@ -234,9 +278,9 @@ export const techStacks = [
   //   width: 30,
   // },
   {
-    key: 'xampp',
-    name: 'XAMPP',
-    url: 'https://www.apachefriends.org/',
+    key: "xampp",
+    name: "XAMPP",
+    url: "https://www.apachefriends.org/",
     icon: xampp,
     height: 30,
     width: 30,
@@ -250,17 +294,17 @@ export const techStacks = [
   //   width: 50,
   // },
   {
-    key: 'mysql',
-    name: 'MySQL',
-    url: 'https://www.mysql.com/',
+    key: "mysql",
+    name: "MySQL",
+    url: "https://www.mysql.com/",
     icon: mysql,
     height: 30,
     width: 40,
   },
   {
-    key: 'mongodb',
-    name: 'MongoDB',
-    url: 'https://www.mongodb.com/',
+    key: "mongodb",
+    name: "MongoDB",
+    url: "https://www.mongodb.com/",
     icon: mongodb,
     height: 35,
     width: 18,
@@ -282,83 +326,82 @@ export const techStacks = [
   //   width: 30,
   // },
   {
-    key: 'git',
-    name: 'GIT',
-    url: 'https://git-scm.com/',
+    key: "git",
+    name: "GIT",
+    url: "https://git-scm.com/",
     icon: git,
     height: 30,
     width: 30,
   },
   {
-    key: 'github_desktop',
-    name: 'GITHUB DESKTOP',
-    url: 'https://desktop.github.com/',
+    key: "github_desktop",
+    name: "GITHUB DESKTOP",
+    url: "https://desktop.github.com/",
     icon: githubLogo,
     height: 30,
     width: 30,
   },
   {
-    key: 'postman',
-    name: 'POSTMAN',
-    url: 'https://postman.com/',
+    key: "postman",
+    name: "POSTMAN",
+    url: "https://postman.com/",
     icon: postman,
     height: 30,
     width: 30,
   },
   {
-    key: 'figma',
-    name: 'FIGMA',
-    url: 'https://www.figma.com/',
+    key: "figma",
+    name: "FIGMA",
+    url: "https://www.figma.com/",
     icon: figma,
     height: 30,
     width: 30,
   },
   {
-    key: 'photoshop',
-    name: 'Photoshop',
-    url: 'https://www.photoshop.com/en',
+    key: "photoshop",
+    name: "Photoshop",
+    url: "https://www.photoshop.com/en",
     icon: photoshop,
     height: 30,
     width: 30,
   },
   {
-    key: 'illustrator',
-    name: 'Illustrator',
-    url: 'https://www.adobe.com/in/products/illustrator.html',
+    key: "illustrator",
+    name: "Illustrator",
+    url: "https://www.adobe.com/in/products/illustrator.html",
     icon: illustrator,
     height: 30,
     width: 30,
   },
-]
-
+];
 
 export const projectsData = [
   {
-    key: 'lodong',
-    name: 'Lodong.co.kr',
+    key: "lodong",
+    name: "Lodong.co.kr",
     img: lodong,
-    desc: 'A landing page for a tech company',
-    url: 'https://lodongsite.vercel.app',
-    stack: ['HTML5', 'NEXT.JS', 'Tailwind CSS'],
-    github: ''
+    desc: "A landing page for a tech company",
+    url: "https://lodongsite.vercel.app",
+    stack: ["HTML5", "NEXT.JS", "Tailwind CSS"],
+    github: "https://github.com/ezmichaely/lodongsite_",
   },
   {
-    key: 'jungnanum',
-    name: 'Jungnanum',
+    key: "jungnanum",
+    name: "Jungnanum",
     img: jungnanum,
-    desc: 'A landing page with complete an admin management system. This facilitated the addition of products and incorporated a personality test questionnaire.',
-    url: 'http://210.99.223.38:20004/',
-    stack: ['HTML5', 'CSS3', 'BOOTSTRAP', 'JAVASCRIPT'],
-    github: ''
+    desc: "A landing page with complete an admin management system. This facilitated the addition of products and incorporated a personality test questionnaire. (already submitted to client using vanilla js, practice converting it in nextjs (unfinished))",
+    url: "https://jungnanum-git-main-ezmichaely.vercel.app/register",
+    stack: ["HTML5", "CSS3", "BOOTSTRAP", "SASS", "JAVASCRIPT"],
+    github: "https://github.com/ezmichaely/ld.jungnanum",
   },
   {
-    key: 'supermandiary',
-    name: 'Superman Diary',
+    key: "supermandiary",
+    name: "Superman Diary",
     img: supermanDairy,
-    desc: 'This system encompassed communication management, constructor and technician team oversight, order and contract management, technician scheduling, and a content management system. It even featured a raffle management system to award prizes to various tenants.',
-    url: 'https://godofhouse.co.kr/',
-    stack: ['REACT.JS', 'EXPRESS.S', 'NODE.JS', 'FIREBASE', 'SOCKET.IO'],
-    github: ''
+    desc: "This system encompassed communication management, constructor and technician team oversight, order and contract management, technician scheduling, and a content management system. It even featured a raffle management system to award prizes to various tenants.",
+    // url: "https://godofhouse.co.kr/",
+    stack: ["REACT.JS", "EXPRESS.S", "NODE.JS", "FIREBASE", "SOCKET.IO"],
+    github: "https://github.com/ezmichaely/ld.supermandiary",
   },
   // {
   //   key: 'garbagecollection',
@@ -378,40 +421,37 @@ export const projectsData = [
   //   stack: ['HTML5', 'CSS3', 'BOOTSTRAP', 'JAVASCRIPT', 'PHP', 'MYSQL'],
   //   github: ''
   // },
-  // {
-  //   key: 'norsuelcms',
-  //   name: 'NORSU-eLCMS',
-  //   img: lodong,
-  //   desc: '',
-  //   url: 'http://210.99.223.38:20004/',
-  //   stack: ['HTML5', 'CSS3', 'BOOTSTRAP', 'JAVASCRIPT', 'PHP', 'MYSQL'],
-  //   github: ''
-  // },
-]
-
+  {
+    key: "norsuelcms",
+    name: "NORSU-eLCMS",
+    img: norsuelcms,
+    desc: "An elearning platform for students with social media / content management. This System also includes student assignment, quizzes and more management.",
+    // url: "http://210.99.223.38:20004/",
+    stack: ["HTML5", "CSS3", "BOOTSTRAP", "JAVASCRIPT", "PHP", "MYSQL"],
+    github: "https://github.com/ezmichaely/norsu_elcms",
+  },
+];
 
 export const galleryDesigns = [
   // { src: bvp, key: 'bvp' },
-  { src: desFiesta, key: 'desFiesta' },
+  { src: desFiesta, key: "desFiesta" },
   // { src: desXmas1, key: 'desXmas1' },
   // { src: desXmas2, key: 'desXmas2' },
-  { src: dwightTarp, key: 'dwightTarp' },
-  { src: dwightTshirt, key: 'dwightTshirt' },
-  { src: infoSteamos, key: 'infoSteamos' },
+  { src: dwightTarp, key: "dwightTarp" },
+  { src: dwightTshirt, key: "dwightTshirt" },
+  { src: infoSteamos, key: "infoSteamos" },
   // { src: infoWin10, key: 'infoWin10' },
-  { src: logo_ducking, key: 'logo_ducking' },
-  { src: logo_farmigs, key: 'logo_farmigs' },
+  { src: logo_ducking, key: "logo_ducking" },
+  { src: logo_farmigs, key: "logo_farmigs" },
   // { src: logo_francyn, key: 'logo_francyn' },
-  { src: logo_rems, key: 'logo_rems' },
+  { src: logo_rems, key: "logo_rems" },
   // { src: logo_tayoktok, key: 'logo_tayoktok' },
   // { src: tarpBday, key: 'tarpBday' },
   // { src: tarpFiesta, key: 'tarpFiesta' },
   // { src: tayok_drinks, key: 'tayok_drinks' },
-  { src: tayok_menu, key: 'tayok_menu' },
-  { src: tayok_opening, key: 'tayok_opening' },
+  { src: tayok_menu, key: "tayok_menu" },
+  { src: tayok_opening, key: "tayok_opening" },
   // { src: tayok_tshirt, key: 'tayok_tshirt' },
-  { src: tayok_sessionistas, key: 'tayok_sessionistas' },
-  { src: tayok_vibes, key: 'tayok_vibes' },
-
-
-]
+  { src: tayok_sessionistas, key: "tayok_sessionistas" },
+  { src: tayok_vibes, key: "tayok_vibes" },
+];
